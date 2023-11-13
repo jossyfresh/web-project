dc9c68e799b0e1236f8d4234a380e46d1dbcd35b
Yodahe Ketema <ketemayodahe@gmail.com>
2023-11-13 13:05:50 +0300

    some corrections for the landing page upper part

--- yaya_academy_web/app/page.tsx
+++ yaya_academy_web/app/page.tsx
@@ -10,38 +10,46 @@ import {
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, useAppSelector } from "./Redux/store";
 import NavBar from "@/components/NavBar";
+import { ArrowBigRight, ArrowRight } from "lucide-react";
 
 export default function Home() {
 	const dispatch = useDispatch<AppDispatch>();
 	const login_status = useAppSelector((state) => state.logged_in?.logged_in);
 	return (
-		<div>
+		<div className="">
 			<NavBar />
-			<div className="flex">
+			<div className="flex mb-20 mt-12">
 				{/* Iamge container */}
 				<div className="flex-1 flex items-center justify-center">
-					<Image src="landingImg.svg" alt="placeholder" width="700" height="700" />
+					<Image src="landingImg.svg" alt="placeholder" width="500" height="500" />
 				</div>
 				{/* Text Container */}
 				<div className="flex-1 flex flex-col justify-center">
-					<div className="space-y-10">
+					<div className="space-y-10 mt-10">
 						<div>
-							<p className="text-[80px] font-medium">
+							<p className="text-6xl font-medium">
 								Learn <span className="text-main">online</span>,
-							</p>{" "}
-							<br />{" "}
-							<p className="text-[70px] mt-[-60px] font-medium">
-								{" "}
-								about construction...
 							</p>
+							<p className="text-5xl font-medium">about construction...</p>
 						</div>
-						<p className="text-4xl font-light text-gray-800">
-							Learn the skills you need to build your dream home or start a career in
-							construction.
+						<p className="text-2xl font-light text-gray-800">
+							Learn the skills you need to <span className="text-main">build</span>{" "}
+							your dream home or start a career in construction.
 						</p>
-						<Button variant="main">Explore Courses</Button>
+						<Button variant="main" className="w-52 group">
+							<p className="translate-x-3 group-hover:translate-x-0 transition-all">Explore Courses</p>
+							<ArrowRight className="opacity-0 group-hover:inline-block group-hover:opacity-100 group-hover:translate-x-3 transition-all duration-300 " />
+						</Button>
 					</div>
 				</div>
+			</div>
+			<div className="px-20 flex flex-col items-center">
+				<p className="text-main text-lg">Hello</p>
+				<p>
+					Are you feeling stuck or overwhelmed in your construction skills? Don't
+					worry, I've got your back! Together, we'll work to level up your skills,
+					increase your earning potential, and <span>build</span> a brighter future.
+				</p>
 			</div>
 		</div>
 	);
--- yaya_academy_web/components/ui/button.tsx
+++ yaya_academy_web/components/ui/button.tsx
@@ -18,7 +18,7 @@ const buttonVariants = cva(
 					"bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
 				ghost: "hover:bg-accent hover:text-accent-foreground",
 				link: "text-primary underline-offset-4 hover:underline",
-				main: "bg-main text-primary-foreground shadow hover:bg-main/80",
+				main: "bg-main text-primary-foreground shadow hover:bg-blue-800",
 			},
 			size: {
 				default: "h-9 px-4 py-2",
