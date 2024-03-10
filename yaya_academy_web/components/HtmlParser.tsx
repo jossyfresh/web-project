41c71b12f69c4bba52b60845a067d056f61f8308
Yodahe Ketema <ketemayodahe@gmail.com>
2024-03-10 23:20:37 +0300

    Html parser added on the home page

---
+++ yaya_academy_web/components/HtmlParser.tsx
@@ -0,0 +1,20 @@
+import { home_data_entry_type } from "@/app/data/dumy";
+import React from "react";
+import parse from 'html-react-parser';
+
+function HtmlParser({ data, fontSize = "md", fontWeight = "normal" }: { data: home_data_entry_type, fontSize?: 'sm' | 'md' | 'lg' | 'xl' | '2xl', fontWeight?: 'light' | 'bold' | 'extrabold' | 'extralight' | 'normal' }) {
+    const responsive_text_size_mapper = {
+        'sm': 'sm',
+        'md' : 'sm' ,
+        'lg' : 'md' ,
+        'xl' : 'lg' ,
+        '2xl' : 'xl',
+    }
+    return data["html"] == null ? 
+        <div className={`lg:text-${fontSize} text-${responsive_text_size_mapper[fontSize]} flex-1 text-center font-${fontWeight}`}>
+            {data['en']}
+    </div>
+    : parse(data["html"]!);
+}
+
+export default HtmlParser;
